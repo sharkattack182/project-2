9421e3d246c395c63d55ff712e7be559d3444b1b
Matthew Courtney <matthewdc182@gmail.com>
2020-09-17 12:17:43 -0400

    fixed a ton of problems iwth jeffs help got the ajax methods started and created the users tables to store pokemon

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,7 +27,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/pokemon.html"));
   });
 
-  app.get("/:pokemon", (req, res) => {
+  app.get("/pokemons/:id", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/individual.html"));
   });
 };
