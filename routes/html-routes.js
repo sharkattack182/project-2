9655daedae1cfdc209e98b89e93501c73c57534f
Matthew Courtney <matthewdc182@gmail.com>
2020-09-20 00:19:40 -0400

    added some basic styling cleanned things up with divs using padding and margins also added a nav bar

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/pokemon.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/login", (req, res) => {
